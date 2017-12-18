7491ee756112940578a60e7d904b7705ef2da31f
yu <18600757741@163.com>
2017-12-18 09:56:49 +0800

    网易云信简单配置

---
+++ SuperDancer/Supports/AppDelegate+NIM.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate+NIM.h
+//  SuperDancer
+//
+//  Created by yu on 2017/12/16.
+//  Copyright © 2017年 yu. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate (NIM)
+
+@end
---
+++ SuperDancer/Supports/AppDelegate+NIM.m
@@ -0,0 +1,15 @@
+//
+//  AppDelegate+NIM.m
+//  SuperDancer
+//
+//  Created by yu on 2017/12/16.
+//  Copyright © 2017年 yu. All rights reserved.
+//
+
+#import "AppDelegate+NIM.h"
+
+@implementation AppDelegate (NIM)
+
+
+
+@end
