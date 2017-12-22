df9ff393367a2f9b043e7df2ebb103971947da20
yu <yu@shanchangdeMacBook-Pro.local>
2017-12-22 13:59:37 +0800

    舞队管理

---
+++ SuperDancer/Classes/Chat/Controller/舞队管理/Controller/TeamManageViewController.h
@@ -0,0 +1,13 @@
+//
+//  TeamManageViewController.h
+//  SuperDancer
+//
+//  Created by yu on 2017/12/22.
+//  Copyright © 2017年 yu. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface TeamManageViewController : BaseViewController
+
+@end
