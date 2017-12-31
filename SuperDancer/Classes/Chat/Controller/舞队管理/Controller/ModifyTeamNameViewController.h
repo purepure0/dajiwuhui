e983125aad1ee34488291d172d42bbefcec340d9
ysc437clear <you@example.com>
2018-01-01 00:28:41 +0800

    修改群公告结构、舞队管理详情页布局、增加修改群名称

---
+++ SuperDancer/Classes/Chat/Controller/舞队管理/Controller/ModifyTeamNameViewController.h
@@ -0,0 +1,13 @@
+//
+//  ModifyTeamNameViewController.h
+//  SuperDancer
+//
+//  Created by yu on 2017/12/31.
+//  Copyright © 2017年 yu. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface ModifyTeamNameViewController : BaseViewController
+
+@end
