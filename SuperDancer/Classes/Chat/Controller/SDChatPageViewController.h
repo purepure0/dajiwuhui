971b051aa54e6bb1e6a9820b91ebfe08b7ea37fe
ysc437clear <you@example.com>
2018-01-02 15:49:20 +0800

    舞队首页结构调整

---
+++ SuperDancer/Classes/Chat/Controller/SDChatPageViewController.h
@@ -0,0 +1,13 @@
+//
+//  SDChatPageViewController.h
+//  SuperDancer
+//
+//  Created by yu on 2018/1/2.
+//  Copyright © 2018年 yu. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface SDChatPageViewController : BaseViewController
+
+@end
---
+++ SuperDancer/Classes/Chat/Controller/联系人/ContactsViewController.h
@@ -0,0 +1,13 @@
+//
+//  ContactsViewController.h
+//  SuperDancer
+//
+//  Created by yu on 2018/1/2.
+//  Copyright © 2018年 yu. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface ContactsViewController : BaseViewController
+
+@end
