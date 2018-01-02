46ab8a19fe98a23dea9c8f3f8558eb8c31cbbb5f
yu <18600757741@163.com>
2018-01-02 15:47:23 +0800

    会话列表

---
+++ SuperDancer/Classes/Chat/Controller/SessionListViewController.h
@@ -0,0 +1,13 @@
+//
+//  SessionListViewController.h
+//  SuperDancer
+//
+//  Created by yu on 2018/1/2.
+//  Copyright © 2018年 yu. All rights reserved.
+//
+
+#import <NIMKit/NIMKit.h>
+
+@interface SessionListViewController : NIMSessionListViewController
+
+@end
---
+++ SuperDancer/Classes/Chat/Controller/SessionListViewController.m
@@ -0,0 +1,53 @@
+//
+//  SessionListViewController.m
+//  SuperDancer
+//
+//  Created by yu on 2018/1/2.
+//  Copyright © 2018年 yu. All rights reserved.
+//
+
+#import "SessionListViewController.h"
+#import "TeamSessionViewController.h"
+#import "FriendChatViewController.h"
+@interface SessionListViewController ()
+
+@end
+
+@implementation SessionListViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    self.title = @"会话列表";
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+- (void)onSelectedRecent:(NIMRecentSession *)recentSession atIndexPath:(NSIndexPath *)indexPath{
+    if (recentSession.session.sessionType == NIMSessionTypeTeam) {
+        TeamSessionViewController *teamSession = [[TeamSessionViewController alloc] initWithSession:recentSession.session];
+        teamSession.team = [[NIMSDK sharedSDK].teamManager teamById:recentSession.session.sessionId];
+        teamSession.teamID = recentSession.session.sessionId;
+        [self.navigationController pushViewController:teamSession animated:YES];
+    }else if (recentSession.session.sessionType == NIMSessionTypeP2P) {
+        FriendChatViewController *friendChat = [[FriendChatViewController alloc] initWithSession:recentSession.session];
+        friendChat.user = [[NIMSDK sharedSDK].userManager userInfo:recentSession.session.sessionId];
+        [self.navigationController pushViewController:friendChat animated:YES];
+    }
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
